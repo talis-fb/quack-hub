ecb429654a52f3f678378158a5d0fb09d0c7a787
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-03-28 12:42:11 -0300

    fix: rename LocalPassportStrategy and remove unnecessary import of Guard

--- backend/src/core/auth/auth.module.ts
+++ backend/src/core/auth/auth.module.ts
@@ -5,13 +5,12 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { JwtAuthGuard } from './jwt/jwt.guard';
-import { LoginPassportStrategy } from './login/login.strategy';
+import { LoginPassportLocalStrategy } from './login/login.strategy';
 import { AuthRepository, AuthRepositoryImpl } from './auth.repository';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { LoginController } from './login/login.controller';
 import { SignUpController } from './signup/signup.controller';
 import { JwtPassportStrategy } from './jwt/jwt.strategy';
-import { ValidateInputForAuthLocal } from './login/guards/validate-input.guard';
 
 @Module({
   imports: [
@@ -30,10 +29,9 @@ import { ValidateInputForAuthLocal } from './login/guards/validate-input.guard';
       provide: AuthRepository,
       useClass: AuthRepositoryImpl,
     },
-    LoginPassportStrategy,
+    LoginPassportLocalStrategy,
     JwtPassportStrategy,
     PrismaService,
-    ValidateInputForAuthLocal,
 
     // Define this as a Global module so that it can be used in all controllers
     {
