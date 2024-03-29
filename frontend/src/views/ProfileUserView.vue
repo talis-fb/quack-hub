c044bd33aa6f0d04a8714e996c4a065397d9409b
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-29 11:15:31 -0300

    [FRONT] feature(ProfileuserView): tornar html mais semantico 2

--- frontend/src/views/ProfileUserView.vue
+++ frontend/src/views/ProfileUserView.vue
@@ -39,14 +39,14 @@ import { Ellipsis } from 'lucide-vue-next'
     </section>
 
     <aside class="flex flex-col gap-6">
-      <div>
+      <section>
         <h2 class="text-2xl">As pessoas também visualizam</h2>
         <p>Coisas que as pessoas vizualizam aqui</p>
-      </div>
-      <div>
+      </section>
+      <section>
         <h2 class="text-2xl">Projetos em destaque</h2>
         <p>A listagem dos projetos em destaque aqui</p>
-      </div>
+      </section>
     </aside>
   </main>
 </template>
