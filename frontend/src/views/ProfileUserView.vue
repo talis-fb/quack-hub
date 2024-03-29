817160b5e166badc23765eaac04f33f9e8c05949
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-29 12:04:35 -0300

    [FRONT] feature(ProfileuserView): tornar html mais semantico 3

--- frontend/src/views/ProfileUserView.vue
+++ frontend/src/views/ProfileUserView.vue
@@ -4,37 +4,41 @@ import { Ellipsis } from 'lucide-vue-next'
 <template>
   <main class="flex flex-1 relative">
     <section class="w-8/12 relative rounded-md">
-      <figure
-        class="m-3 bg-[url('@/assets//wallpaper-default-user.jpg')] h-48 bg-cover rounded-lg"
-      ></figure>
+      <section>
+        <figure
+          class="m-3 bg-[url('@/assets//wallpaper-default-user.jpg')] h-48 bg-cover rounded-lg"
+        ></figure>
 
-      <figure class="ml-6 mt-3 absolute top-28 flex flex-col items-center justify-center">
-        <img class="max-h-36 rounded-full" src="@/assets/user-icon.jpg" alt="user-icon" />
-        <figcaption class="text-lg">User Name</figcaption>
-      </figure>
+        <figure class="ml-6 mt-3 absolute top-28 flex flex-col items-center justify-center">
+          <img class="max-h-36 rounded-full" src="@/assets/user-icon.jpg" alt="user-icon" />
+          <figcaption class="text-lg">User Name</figcaption>
+        </figure>
 
-      <button class="absolute right-5">
-        <Ellipsis />
-      </button>
+        <button class="absolute right-5">
+          <Ellipsis />
+        </button>
 
-      <section class="min-h-36 mt-32 pl-6 border-b-2 border-slate-400">
-        <p>Cidade, estado, pais + Informações de contato</p>
-        <p class="mt-6"><span>Seguidores</span> | <span>Seguindo</span></p>
+        <div class="min-h-36 mt-32 pl-6 border-b-2 border-slate-400">
+          <p>Cidade, estado, pais + Informações de contato</p>
+          <p class="mt-6"><span>Seguidores</span> | <span>Seguindo</span></p>
+        </div>
       </section>
 
-      <section class="m-5">
-        <h2 class="text-2xl">Sobre</h2>
-        <p>Informações do usuario</p>
-      </section>
+      <section>
+        <div class="m-5">
+          <h2 class="text-2xl">Sobre</h2>
+          <p>Informações do usuario</p>
+        </div>
 
-      <section class="m-5">
-        <h2 class="text-2xl">Atividades</h2>
-        <p>Projetos em que participou</p>
-      </section>
+        <div class="m-5">
+          <h2 class="text-2xl">Atividades</h2>
+          <p>Projetos em que participou</p>
+        </div>
 
-      <section class="m-5">
-        <h2 class="text-2xl">Formação acadêmica</h2>
-        <p>Informações do usuario</p>
+        <div class="m-5">
+          <h2 class="text-2xl">Formação acadêmica</h2>
+          <p>Informações do usuario</p>
+        </div>
       </section>
     </section>
 
