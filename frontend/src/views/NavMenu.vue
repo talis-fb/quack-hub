bcc9b49fa8979255075d4018d2a909ff5cdb7df8
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-30 15:08:41 -0300

    [FRONTEND] feature(NavMenu): utilizar separator

--- frontend/src/views/NavMenu.vue
+++ frontend/src/views/NavMenu.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
 import { metadataRoutes } from '@/router/RoutesConfig'
 import AvatarDefault from '@/assets/user-icon.jpg'
+import Logo from '@/assets/logo.png'
 
 // Icons
 import { Home } from 'lucide-vue-next'
 import { BadgeInfo } from 'lucide-vue-next'
-import { ArrowDown } from 'lucide-vue-next'
+import { ChevronDown } from 'lucide-vue-next'
 
 // Components shadcn-vue
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Separator } from '@/components/ui/separator'
 </script>
 <template>
-  <nav class="flex px-4 items-center justify-between">
+  <nav class="flex px-6 py-2 justify-between">
     <header>
-      <RouterLink :to="metadataRoutes.HOME.path"
-        ><img class="max-h-20" src="../assets/logo.png" alt="quackhub-logo"
-      /></RouterLink>
+      <RouterLink :to="metadataRoutes.HOME.path">
+        <img class="max-h-20" :src="Logo" alt="quackhub-logo" />
+      </RouterLink>
     </header>
 
     <div class="flex md:gap-4 gap-8">
@@ -35,8 +37,10 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
         <p class="md:block hidden">About</p>
       </RouterLink>
 
+      <Separator orientation="vertical" class="w-1" />
+
       <RouterLink
-        class="flex items-center gap-1 text-lg hover:text-blue-500"
+        class="flex flex-col justify-center items-center gap-1 hover:text-blue-500"
         :to="metadataRoutes.USER_PROFILE.path"
       >
         <Avatar>
@@ -45,7 +49,10 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
             <img :src="AvatarDefault" alt="user-avatar-default" />
           </AvatarFallback>
         </Avatar>
-        <ArrowDown class="w-6" />
+        <div class="md:block hidden">
+          <span class="text-sm">Eu</span>
+          <ChevronDown class="inline-block" :size="22" />
+        </div>
       </RouterLink>
     </div>
   </nav>
