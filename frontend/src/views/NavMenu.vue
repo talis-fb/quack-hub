fb5aa9c83922617c58fce8eff6ce9c5319b40cf7
Joab Urbano <joab.uaraujo@gmail.com>
2024-03-30 11:29:58 -0300

    Responsividade do NavMenu

--- frontend/src/views/NavMenu.vue
+++ frontend/src/views/NavMenu.vue
@@ -5,27 +5,29 @@ import { BadgeInfo } from 'lucide-vue-next'
 import { ArrowDown } from 'lucide-vue-next'
 </script>
 <template>
-  <nav class="flex relative m-1">
+  <nav class="flex m-1 justify-between">
     <RouterLink :to="metadataRoutes.HOME.path"
-      ><img class="max-h-20 ml-6" src="../assets/logo.png" alt="quackhub-logo"
+      ><img class="max-h-20 md:ml-6 ml-0" src="../assets/logo.png" alt="quackhub-logo"
     /></RouterLink>
-    <main class="flex gap-6 absolute right-24">
+    <main class="flex gap-6">
       <RouterLink
-        class="flex gap-2 items-center mt-5 text-lg hover:text-blue-700"
+        class="flex gap-2 items-center text-lg hover:text-blue-700"
         :to="metadataRoutes.HOME.path"
       >
         <Home />
         <p>Home</p>
       </RouterLink>
+      
       <RouterLink
-        class="flex gap-2 items-center mt-5 text-lg hover:text-blue-700 pr-9 border-r-4 border-white"
+        class="flex gap-2 items-center text-lg hover:text-blue-700 pr-9 border-r-4 border-slate-300"
         :to="metadataRoutes.ABOUT.path"
       >
         <BadgeInfo />
         <p>About</p>
       </RouterLink>
+
       <RouterLink
-        class="flex gap-2 items-center mt-5 text-lg hover:text-blue-700 mr-9"
+        class="flex items-center text-lg hover:text-blue-700 mr-9"
         :to="metadataRoutes.USER_PROFILE.path"
       >
         <img class="max-h-10 rounded-full" src="@/assets/user-icon.jpg" alt="user-profile" />
