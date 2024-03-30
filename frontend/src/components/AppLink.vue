fdb885d2cbd19600aad892a74b9d09124c5b263b
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-30 14:46:45 -0300

    [FRONTEND] feature(NavMenu): tirar classes sem necesisdade

---
+++ frontend/src/components/AppLink.vue
@@ -0,0 +1,27 @@
+<!-- <script setup lang="ts">
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const props = defineProps({
+  // add @ts-ignore if using TypeScript
+  ...RouterLink.props,
+  inactiveClass: String
+})
+</script>
+<template>
+  <RouterLink v-bind="$props" custom v-slot="{ isActive, href, navigate }">
+    <a
+      v-bind="$attrs"
+      :href="href"
+      @click="navigate"
+      :class="isActive ? activeClass : inactiveClass"
+    >
+      <slot />
+    </a>
+  </RouterLink>
+</template>
+<style></style> -->
--- frontend/src/views/NavMenu.vue
+++ frontend/src/views/NavMenu.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-    import { metadataRoutes } from '@/router/RoutesConfig'
-    import { Home } from 'lucide-vue-next'
-    import { BadgeInfo } from 'lucide-vue-next'
-    import { ArrowDown } from 'lucide-vue-next'
+import { metadataRoutes } from '@/router/RoutesConfig'
+import AvatarDefault from '@/assets/user-icon.jpg'
+
+// Icons
+import { Home } from 'lucide-vue-next'
+import { BadgeInfo } from 'lucide-vue-next'
+import { ArrowDown } from 'lucide-vue-next'
+
+// Components shadcn-vue
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 </script>
 <template>
-  <nav class="flex m-1 justify-between">
-    <RouterLink :to="metadataRoutes.HOME.path"
-      ><img class="max-h-20 md:ml-6 ml-0" src="../assets/logo.png" alt="quackhub-logo"
-    /></RouterLink>
-    <main class="flex md:gap-4 gap-8">
+  <nav class="flex px-4 items-center justify-between">
+    <header>
+      <RouterLink :to="metadataRoutes.HOME.path"
+        ><img class="max-h-20" src="../assets/logo.png" alt="quackhub-logo"
+      /></RouterLink>
+    </header>
+
+    <div class="flex md:gap-4 gap-8">
       <RouterLink
         class="flex gap-2 items-center text-lg hover:text-blue-500"
         :to="metadataRoutes.HOME.path"
@@ -17,9 +26,9 @@
         <Home />
         <p class="md:block hidden">Home</p>
       </RouterLink>
-      
+
       <RouterLink
-        class="flex gap-2 items-center text-lg hover:text-blue-500 pr-9 border-r-2 border-slate-300"
+        class="flex gap-2 items-center text-lg hover:text-blue-500"
         :to="metadataRoutes.ABOUT.path"
       >
         <BadgeInfo />
@@ -27,13 +36,18 @@
       </RouterLink>
 
       <RouterLink
-        class="flex items-center text-lg hover:text-blue-500 md:mr-9"
+        class="flex items-center gap-1 text-lg hover:text-blue-500"
         :to="metadataRoutes.USER_PROFILE.path"
       >
-        <img class="max-h-10 rounded-full" src="@/assets/user-icon.jpg" alt="user-profile" />
-        <ArrowDown class="w-10" />
+        <Avatar>
+          <AvatarImage src="https://urlnaoencontrada.com/radix-vue.png" alt="user-avatar" />
+          <AvatarFallback>
+            <img :src="AvatarDefault" alt="user-avatar-default" />
+          </AvatarFallback>
+        </Avatar>
+        <ArrowDown class="w-6" />
       </RouterLink>
-    </main>
+    </div>
   </nav>
 </template>
 <style></style>
