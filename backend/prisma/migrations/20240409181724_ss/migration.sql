acaa961032b7f75137a0c151a80c580a92989ae7
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-04-09 15:21:57 -0300

    feat(crud-projects): squash migrations

--- backend/prisma/migrations/20240409181724_ss/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Experience" DROP CONSTRAINT "Experience_projectId_fkey";
-
--- AlterTable
-ALTER TABLE "Experience" ALTER COLUMN "projectId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Experience" ADD CONSTRAINT "Experience_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- backend/prisma/migrations/20240409182135_correcoes_projeto_e_experiencia/migration.sql
+++ backend/prisma/migrations/20240409182135_correcoes_projeto_e_experiencia/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - You are about to drop the column `experienceId` on the `Project` table. All the data in the column will be lost.
-  - Added the required column `projectId` to the `Experience` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -12,10 +11,10 @@ ALTER TABLE "Project" DROP CONSTRAINT "Project_experienceId_fkey";
 DROP INDEX "Project_experienceId_key";
 
 -- AlterTable
-ALTER TABLE "Experience" ADD COLUMN     "projectId" INTEGER NOT NULL;
+ALTER TABLE "Experience" ADD COLUMN     "projectId" INTEGER;
 
 -- AlterTable
 ALTER TABLE "Project" DROP COLUMN "experienceId";
 
 -- AddForeignKey
-ALTER TABLE "Experience" ADD CONSTRAINT "Experience_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
