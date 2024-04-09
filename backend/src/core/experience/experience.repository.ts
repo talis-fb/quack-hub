a4ea9acd6cb3c00af5d1ce85ab208b268cd1241f
Joab Urbano <joab.uaraujo@gmail.com>
2024-04-09 20:23:48 -0300

    Criação abstract repository

--- backend/src/core/experience/experience.repository.ts
+++ backend/src/core/experience/experience.repository.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { ExperienceData, ExperienceEntity } from "../user/user.entity";
+import { PrismaService } from "src/common/prisma/prisma.service";
 
 export abstract class ExperienceRepository {
-
+    abstract getExperienceById(id: number): Promise<ExperienceEntity | null>;
+    abstract createExperience(experience: ExperienceData): Promise<ExperienceEntity>;
+    abstract updateExperience(is: number, experience: Partial<ExperienceData>):Promise<ExperienceEntity | null>;
 }
 
 @Injectable
 export class ExperienceRepositoryImpl implements ExperienceRepository {
+    constructor(private prisma: PrismaService) {}
 
+    async async getExperienceById(id: number): Promise<ExperienceEntity> {
+        
+    }
 }
