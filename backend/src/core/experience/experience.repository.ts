a8690c35988f811a6f70248222830b2d70aa2815
Joab Urbano <joab.uaraujo@gmail.com>
2024-04-09 20:39:47 -0300

    Criação repository

--- backend/src/core/experience/experience.repository.ts
+++ backend/src/core/experience/experience.repository.ts
@@ -1,18 +1,52 @@
-import { Injectable } from "@nestjs/common";
-import { ExperienceData, ExperienceEntity } from "../user/user.entity";
-import { PrismaService } from "src/common/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { ExperienceData, ExperienceEntity } from '../user/user.entity';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 export abstract class ExperienceRepository {
-    abstract getExperienceById(id: number): Promise<ExperienceEntity | null>;
-    abstract createExperience(experience: ExperienceData): Promise<ExperienceEntity>;
-    abstract updateExperience(is: number, experience: Partial<ExperienceData>):Promise<ExperienceEntity | null>;
+  abstract getExperienceById(id: number): Promise<ExperienceEntity | null>;
+  abstract createExperience(
+    experience: ExperienceData,
+  ): Promise<ExperienceEntity>;
+  abstract updateExperience(
+    id: number,
+    experience: Partial<ExperienceData>,
+  ): Promise<ExperienceEntity | null>;
 }
 
 @Injectable
 export class ExperienceRepositoryImpl implements ExperienceRepository {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async async getExperienceById(id: number): Promise<ExperienceEntity> {
-        
-    }
+  async getExperienceById(id: number): Promise<ExperienceEntity> {
+    const output = await this.prisma.experience.findUnique({
+      where: {
+        id,
+      },
+    });
+    return output;
+  }
+
+  async createExperience(
+    experience: ExperienceData,
+  ): Promise<ExperienceEntity> {
+    const output = await this.prisma.experience.create({
+      data: {
+        ...experience,
+      },
+    });
+    return output;
+  }
+
+  async updateExperience(
+    id: number,
+    experience: Partial<ExperienceData>,
+  ): Promise<ExperienceEntity> {
+    const output = await this.prisma.experience.update({
+      where: {
+        id,
+      },
+      data: experience,
+    });
+    return output;
+  }
 }
