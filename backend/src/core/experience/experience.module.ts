2ec9e1e5be264b607b75244a3c1767d7f3fcf21b
Joab Urbano <joab.uaraujo@gmail.com>
2024-04-10 18:41:58 -0300

    Configuração do experienceModule

--- backend/src/core/experience/experience.module.ts
+++ backend/src/core/experience/experience.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ExperienceController } from './experience.controller';
-import { ExperienceService } from './experience.service';
-import { ExperienceRepository } from './experience.repository';
+import { ExperienceService, ExperienceServiceImpl } from './experience.service';
+import {
+  ExperienceRepository,
+  ExperienceRepositoryImpl,
+} from './experience.repository';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Module({
   controllers: [ExperienceController],
   providers: [
-    ExperienceService,
     PrismaService,
-    //ExperienceRepository,
-  ]
+    {
+      provide: ExperienceService,
+      useClass: ExperienceServiceImpl,
+    },
+    {
+      provide: ExperienceRepository,
+      useClass: ExperienceRepositoryImpl,
+    },
+  ],
 })
 export class ExperienceModule {}
--- backend/src/core/experience/experience.service.ts
+++ backend/src/core/experience/experience.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExperienceService {}
+export abstract class ExperienceService {}
+
+@Injectable()
+export class ExperienceServiceImpl implements ExperienceService {}
