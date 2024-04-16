50e3a362882dc59be3eabac0acf620ea1d43a03c
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-04-16 13:34:23 -0300

    [FRONTEND] feature(ProfileUserView): adicionar section de experiencia profissional

--- frontend/src/views/ProfileUserView.vue
+++ frontend/src/views/ProfileUserView.vue
@@ -66,6 +66,37 @@ import { Ellipsis, Plus, Pencil } from 'lucide-vue-next'
       </section>
 
       <section class="flex flex-col gap-3 p-5 bg-secondary rounded-md">
+        <div class="flex items-center">
+          <h2 class="text-2xl mr-auto">Experiências profissionais</h2>
+
+          <AppDialog>
+            <template #trigger>
+              <Button variant="outline" size="icon" class="bg-transparent hover:bg-black/40">
+                <Plus class="w-5 h-5" />
+              </Button>
+            </template>
+            <template #title> Adicionar experiência profissional </template>
+            <template #description>
+              Adicione suas experiências profissionais para que outros usuários possam ver seu
+              perfil profissional.
+            </template>
+            <template #main>
+              <ExperienceForm
+                title-label="Título"
+                title-placeholder="Ex.: Desenvolvedor Backend"
+                type="PROFESSIONAL"
+              />
+            </template>
+          </AppDialog>
+
+          <Button variant="outline" size="icon" class="bg-transparent hover:bg-black/40">
+            <Pencil class="w-5 h-5" />
+          </Button>
+        </div>
+        <p>Informações do usuario</p>
+      </section>
+
+      <section class="flex flex-col gap-3 p-5 bg-secondary rounded-md">
         <div>
           <h2 class="text-2xl">Sobre</h2>
           <p>Informações do usuario</p>
