1b84aedf813db1868463e155d9370a15a1fa9f64
Joab Urbano <joab.uaraujo@gmail.com>
2024-04-17 21:53:10 -0300

    Tratamento dos códigos de erro no filter

--- backend/src/excpetions/controller/FilterServiceException.ts
+++ backend/src/excpetions/controller/FilterServiceException.ts
@@ -6,6 +6,23 @@ import {
 } from '@nestjs/common';
 import { ServiceException } from '../service/ServiceException';
 import { ServiceClientKnownRequestException } from '../service/ServiceClientKnownRequestException';
+import { ServiceClientValidationException } from '../service/ServiceClientValidationException';
+import { ServiceClientInitializationException } from '../service/ServiceClientInitializationException';
+import { ServiceNotFoundException } from '../service/ServiceNotFoundException';
+
+function verifyStatusCode(exception: ServiceException) {
+  if (exception instanceof ServiceClientKnownRequestException) {
+    return HttpStatus.BAD_REQUEST;
+  } else if (exception instanceof ServiceClientValidationException) {
+    return HttpStatus.UNPROCESSABLE_ENTITY;
+  } else if (exception instanceof ServiceClientInitializationException) {
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  } else if (exception instanceof ServiceNotFoundException) {
+    return HttpStatus.NOT_FOUND;
+  } else {
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+}
 
 @Catch(ServiceException)
 export class FilterServiceException implements ExceptionFilter {
@@ -13,18 +30,11 @@ export class FilterServiceException implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
-    if (exception instanceof ServiceClientKnownRequestException) {
-      const status = HttpStatus.BAD_REQUEST;
-      response.status(status).json({
-        statusCode: status,
-        message: exception.message,
-      });
-    } else {
-      const status = HttpStatus.INTERNAL_SERVER_ERROR;
-      response.status(status).json({
-        statusCode: status,
-        message: exception.message,
-      });
-    }
+    const status = verifyStatusCode(exception);
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+    });
   }
 }
