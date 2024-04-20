804e7dacaa8195a23ee511c35b2f55408de8064d
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-04-20 14:48:13 -0300

    [FRONTEND] feature(Experiences): adicionar alerta caso usuario nao tenha experienncias

--- frontend/src/components/ExperiencesList.vue
+++ frontend/src/components/ExperiencesList.vue
@@ -12,6 +12,7 @@ import { storeToRefs } from 'pinia'
 
 // Shadcn-vue components
 import { Separator } from '@/components/ui/separator'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 export interface ExperienceListProps {
   userId: number
@@ -31,10 +32,18 @@ await experienceStore.getExperiennces(props.userId, props.type)
 
 <template>
   <div class="flex flex-col gap-5">
-    <div v-for="(experience, index) in experiences[type]">
+    <div v-if="experiences[type].length" v-for="(experience, index) in experiences[type]">
       <Separator class="mb-4 bg-white/30" />
       <ExperienceItem :experience="experience" />
     </div>
+
+    <Alert v-else>
+      <AlertTitle
+        >Usuário sem experiências
+        {{ props.type == 'ACADEMIC' ? 'acadêmicas' : 'profisisonais' }}</AlertTitle
+      >
+      <!-- <AlertDescription></AlertDescription> -->
+    </Alert>
   </div>
 </template>
 
