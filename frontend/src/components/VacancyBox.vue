95b7a1e056e49a0c2b65e30af2c79e7d9318ba22
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-04-25 20:31:00 -0300

    [FRONTEND] feature(VacancyBox): estilizar botao pra editar vaga

--- frontend/src/components/VacancyBox.vue
+++ frontend/src/components/VacancyBox.vue
@@ -7,7 +7,11 @@ import { vacancyLabelState, projectStateLabel } from '@/utils/labels'
 
 // Shadcn-vue components
 import { Badge } from '@/components/ui/badge'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
 
+// Icons
+import { MoreHorizontalIcon } from 'lucide-vue-next'
 export interface IVacancyBoxProps {
   vacancy: IVacancyEntity
 }
@@ -19,9 +23,23 @@ defineProps<IVacancyBoxProps>()
   <div
     class="p-4 transition-all delay-50 flex flex-col border rounded-md gap-2 shadow-lg hover:shadow-primary/50"
   >
-    <span class="text-2xl font-bold">
-      {{ vacancy.title }}
-    </span>
+    <header class="flex space-x-2 items-center justify-between relative">
+      <span class="text-2xl font-bold">
+        {{ vacancy.title }}
+      </span>
+      <Popover>
+        <PopoverTrigger as-child>
+          <MoreHorizontalIcon class="cursor-pointer" />
+        </PopoverTrigger>
+        <PopoverContent class="max-w-[150px] p-0">
+          <div class="flex flex-col">
+            <div @click.stop="" class="cursor-pointer p-3 text-center hover:bg-muted">Editar</div>
+            <div @click.stop="" class="cursor-pointer p-3 text-center hover:bg-muted">Remover</div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </header>
+
     <span>
       {{ vacancy.description }}
     </span>
