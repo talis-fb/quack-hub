2455b34a3871df2c318556e8dbb004a8ff50bc6e
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-04-29 21:54:56 -0300

    [FRONTEND] feature(ProjectForm): adicionar campo methodologies

--- frontend/src/components/ProjectForm.vue
+++ frontend/src/components/ProjectForm.vue
@@ -6,7 +6,7 @@ import DefaultProjectIcon from '@/assets/DefaultProjectIcon.jpg'
 import { projectStateLabel } from '@/utils/labels'
 
 // Zod
-import { useForm } from 'vee-validate'
+import { useFieldArray, useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
 import * as z from 'zod'
 
@@ -36,10 +36,11 @@ import AppDialog from '@/components/AppDialog.vue'
 import GithubProjectImport from '@/components/GithubProjectImport.vue'
 
 // Icons
-import { Calendar as CalendarIcon, ImageIcon, Github, Linkedin } from 'lucide-vue-next'
+import { Calendar as CalendarIcon, ImageIcon, Github, Linkedin, Plus, X } from 'lucide-vue-next'
 import { type IProjectEntity, StateProjectValues } from '../entites/IProject'
 import type { ICreateProject } from '@/apis/project/types/ICreateProject'
 import type { IProjectGithubResponse } from '@/apis/github/github.api'
+import { ref } from 'vue'
 
 export interface IProjectFormProps {
   project?: IProjectEntity
@@ -91,7 +92,13 @@ const formSchema = toTypedSchema(
       .url({ message: 'Esse não é um link válido.' })
       .nullish()
       .or(z.literal(''))
-      .transform((e) => (e === '' ? null : e))
+      .transform((e) => (e === '' ? null : e)),
+    methodologies: z
+      .string()
+      .min(2, {
+        message: 'A metodologia deve ter no mínimo 2 caracteres.'
+      })
+      .array()
   })
 )
 
@@ -107,7 +114,8 @@ form.setValues({
   state: props.project?.state,
   startDate: props.project?.startDate,
   endDate: props.project?.endDate,
-  logoUrl: props.project?.logoUrl
+  logoUrl: props.project?.logoUrl,
+  methodologies: props.project?.methodologies
 })
 
 const onSubmit = form.handleSubmit(async (values) => {
@@ -121,183 +129,216 @@ const handleProjectImported = (data: IProjectGithubResponse) => {
     startDate: new Date(data.created_at)
   })
 }
+
+const { remove, push, fields } = useFieldArray('methodologies')
 </script>
 
 <template>
-  <p>Importar de:</p>
-  <div class="flex justify-center space-x-2">
-    <AppDialog>
-      <template #trigger>
-        <Button variant="outline" size="icon" class="rounded-full">
-          <Github />
-        </Button>
-      </template>
-      <template #title> Importar projeto pelo Github </template>
-      <template #description> Importe seu projeto pelo GitHub e economize seu tempo! </template>
-      <template #main>
-        <GithubProjectImport @imported="handleProjectImported" />
-      </template>
-    </AppDialog>
+  <div class="p-3 max-h-[600px] overflow-auto">
+    <p>Importar de:</p>
+    <div class="flex justify-center space-x-2">
+      <AppDialog>
+        <template #trigger>
+          <Button variant="outline" size="icon" class="rounded-full">
+            <Github />
+          </Button>
+        </template>
+        <template #title> Importar projeto pelo Github </template>
+        <template #description> Importe seu projeto pelo GitHub e economize seu tempo! </template>
+        <template #main>
+          <GithubProjectImport @imported="handleProjectImported" />
+        </template>
+      </AppDialog>
 
-    <Button variant="outline" size="icon" class="rounded-full">
-      <Linkedin />
-    </Button>
-  </div>
+      <Button variant="outline" size="icon" class="rounded-full">
+        <Linkedin />
+      </Button>
+    </div>
 
-  <form @submit="onSubmit" class="w-full flex flex-col gap-4">
-    <FormField v-slot="{ componentField }" name="title">
-      <FormItem>
-        <FormLabel>Título</FormLabel>
-        <FormControl>
-          <Input
-            type="text"
-            placeholder="Ex.: Duckchat"
-            v-bind="componentField"
-            autocomplete="title"
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="summary">
-      <FormItem>
-        <FormLabel>Resumo</FormLabel>
-
-        <FormControl>
-          <Input
-            placeholder="Ex.: Cópia do discord"
-            v-bind="componentField"
-            autocomplete="summary"
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="about">
-      <FormItem>
-        <FormLabel>Sobre</FormLabel>
-
-        <FormLabel />
-        <FormControl>
-          <Textarea
-            placeholder="Ex.: Projeto baseado nas funcionalidades básicas de chat em tempo real."
-            v-bind="componentField"
-            class="resize-none"
-            autocomplete="about"
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="sector">
-      <FormItem>
-        <FormLabel>Setor</FormLabel>
-
-        <FormLabel />
-        <FormControl>
-          <Input placeholder="Ex.: Software" v-bind="componentField" autocomplete="sector" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="state">
-      <FormItem>
-        <FormLabel>Status</FormLabel>
-
-        <Select v-bind="componentField">
+    <form @submit="onSubmit" class="w-full flex flex-col gap-4">
+      <FormField v-slot="{ componentField }" name="title">
+        <FormItem>
+          <FormLabel>Título</FormLabel>
           <FormControl>
-            <SelectTrigger>
-              <SelectValue placeholder="Selecione o status do projeto" />
-            </SelectTrigger>
-          </FormControl>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem v-for="item in Object.entries(projectStateLabel)" :value="item[0]">
-                {{ item[1] }}
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField, value }" name="startDate">
-      <FormItem class="flex flex-col">
-        <FormLabel>Início</FormLabel>
-
-        <Popover>
-          <PopoverTrigger as-child>
-            <FormControl>
-              <Button variant="outline" :class="cn(!value && 'text-muted-foreground')">
-                <span>{{ value ? formatDateInFull(value) : 'Início' }}</span>
-                <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
-              </Button>
-            </FormControl>
-            <FormMessage />
-          </PopoverTrigger>
-          <PopoverContent class="p-0">
-            <Calendar v-bind="componentField" />
-          </PopoverContent>
-        </Popover>
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField, value }" name="endDate">
-      <FormItem class="flex flex-col">
-        <FormLabel>Fim</FormLabel>
-
-        <Popover>
-          <PopoverTrigger as-child>
-            <FormControl>
-              <Button variant="outline" :class="cn(!value && 'text-muted-foreground')">
-                <span>{{ value ? formatDateInFull(value) : 'Fim' }}</span>
-                <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
-              </Button>
-            </FormControl>
-            <FormMessage />
-          </PopoverTrigger>
-          <PopoverContent class="p-0">
-            <Calendar v-bind="componentField" />
-          </PopoverContent>
-        </Popover>
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="logoUrl">
-      <FormItem>
-        <FormLabel>Logo</FormLabel>
-        <FormControl>
-          <div class="relative w-full max-w-sm items-center">
             <Input
               type="text"
-              id=""
-              placeholder="URL da image..."
-              class="pl-10"
+              placeholder="Ex.: Duckchat"
               v-bind="componentField"
+              autocomplete="title"
             />
-            <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
-              <ImageIcon class="size-6 text-muted-foreground" />
-            </span>
-          </div>
-          <Avatar class="size-20">
-            <AvatarImage :src="componentField.modelValue ?? ''" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
-            <AvatarFallback>
-              <img :src="DefaultProjectIcon" alt="project-logo" />
-            </AvatarFallback>
-          </Avatar>
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+      <FormField v-slot="{ componentField }" name="summary">
+        <FormItem>
+          <FormLabel>Resumo</FormLabel>
 
-    <Button type="submit" class="w-full">Salvar</Button>
-  </form>
+          <FormControl>
+            <Input
+              placeholder="Ex.: Cópia do discord"
+              v-bind="componentField"
+              autocomplete="summary"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="about">
+        <FormItem>
+          <FormLabel>Sobre</FormLabel>
+
+          <FormLabel />
+          <FormControl>
+            <Textarea
+              placeholder="Ex.: Projeto baseado nas funcionalidades básicas de chat em tempo real."
+              v-bind="componentField"
+              class="resize-none"
+              autocomplete="about"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="sector">
+        <FormItem>
+          <FormLabel>Setor</FormLabel>
+
+          <FormLabel />
+          <FormControl>
+            <Input placeholder="Ex.: Software" v-bind="componentField" autocomplete="sector" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="state">
+        <FormItem>
+          <FormLabel>Status</FormLabel>
+
+          <Select v-bind="componentField">
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione o status do projeto" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem v-for="item in Object.entries(projectStateLabel)" :value="item[0]">
+                  {{ item[1] }}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField, value }" name="startDate">
+        <FormItem class="flex flex-col">
+          <FormLabel>Início</FormLabel>
+
+          <Popover>
+            <PopoverTrigger as-child>
+              <FormControl>
+                <Button variant="outline" :class="cn(!value && 'text-muted-foreground')">
+                  <span>{{ value ? formatDateInFull(value) : 'Início' }}</span>
+                  <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+              <FormMessage />
+            </PopoverTrigger>
+            <PopoverContent class="p-0">
+              <Calendar v-bind="componentField" />
+            </PopoverContent>
+          </Popover>
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField, value }" name="endDate">
+        <FormItem class="flex flex-col">
+          <FormLabel>Fim</FormLabel>
+
+          <Popover>
+            <PopoverTrigger as-child>
+              <FormControl>
+                <Button variant="outline" :class="cn(!value && 'text-muted-foreground')">
+                  <span>{{ value ? formatDateInFull(value) : 'Fim' }}</span>
+                  <CalendarIcon class="ms-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+              <FormMessage />
+            </PopoverTrigger>
+            <PopoverContent class="p-0">
+              <Calendar v-bind="componentField" />
+            </PopoverContent>
+          </Popover>
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="logoUrl">
+        <FormItem>
+          <FormLabel>Logo</FormLabel>
+          <FormControl>
+            <div class="relative w-full max-w-sm items-center">
+              <Input
+                type="text"
+                id=""
+                placeholder="URL da image..."
+                class="pl-10"
+                v-bind="componentField"
+              />
+              <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+                <ImageIcon class="size-6 text-muted-foreground" />
+              </span>
+            </div>
+            <Avatar class="size-20">
+              <AvatarImage :src="componentField.modelValue ?? ''" />
+
+              <AvatarFallback>
+                <img :src="DefaultProjectIcon" alt="project-logo" />
+              </AvatarFallback>
+            </Avatar>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <Button @click="() => push('')" type="button" class="self-start" variant="secondary">
+        <Plus />
+        Adicione Metodologias
+      </Button>
+
+      <FormField
+        v-for="(field, index) in fields"
+        v-slot="{ componentField }"
+        :name="'methodologies.' + index"
+      >
+        <FormItem>
+          <FormLabel>Metodologia {{ index + 1 }}</FormLabel>
+          <FormControl>
+            <div class="flex space-x-2">
+              <Button @click="() => remove(index)" type="button" variant="ghost" size="icon">
+                <X :size="20" />
+              </Button>
+              <Input
+                type="text"
+                placeholder="Ex.: Typescript"
+                v-bind="componentField"
+                autocomplete="methodologies"
+              />
+            </div>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <Button type="submit" class="w-full">Salvar</Button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
