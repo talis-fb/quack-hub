283755e6a3d7d5cf868b9eb5b8dfabc1788e6467
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-06 15:23:57 -0300

    [FRONTEND] feature(CommentItem): implementar deleção

---
+++ frontend/src/components/CommentItem.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// Images
+import DefaultUserIcon from '@/assets/DefaultUserIcon.jpg';
+
+// Shadcn-vue components
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+// Pinia imports
+import { usePostStore } from '@/stores/post'
+import { storeToRefs } from 'pinia'
+
+// Types
+import { type ICommentEntity } from '../entites/IComment'
+
+export interface CommentItemProps {
+  comment: ICommentEntity
+}
+
+const props = defineProps<CommentItemProps>()
+
+const postStore = usePostStore()
+</script>
+
+<template>
+  <div class="flex px-2 py-3 gap-2">
+    <Avatar class="w-12 h-12">
+      <AvatarImage :src="comment.User.avatarUrl ?? ''" />
+
+      <AvatarFallback>
+        <img :src="DefaultUserIcon" alt="avatar_user" />
+      </AvatarFallback>
+    </Avatar>
+
+    <div class="flex-1 space-y-2">
+      <p>
+        {{ comment.User.name }}
+      </p>
+      <p>
+        {{ comment.content }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/components/CommentsList.vue
+++ frontend/src/components/CommentsList.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-// Images
-import DefaultUserIcon from '@/assets/DefaultUserIcon.jpg'
+// App components
+import CommentItem from '@/components/CommentItem.vue'
 
 // Shadcn-vue components
 import { Separator } from '@/components/ui/separator'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 // Vue imports
+
 import { usePostStore } from '@/stores/post'
 import { storeToRefs } from 'pinia'
 
@@ -25,24 +25,7 @@ await postStore.fetchComments(props.postId)
 <template>
   <div class="flex flex-col">
     <div v-for="comment in comments">
-      <div class="flex px-2 py-3 gap-2">
-        <Avatar class="w-12 h-12">
-          <AvatarImage :src="comment.User.avatarUrl ?? ''" />
-
-          <AvatarFallback>
-            <img :src="DefaultUserIcon" alt="avatar_user" />
-          </AvatarFallback>
-        </Avatar>
-
-        <div class="flex-1 space-y-2">
-          <p>
-            {{ comment.User.name }}
-          </p>
-          <p>
-            {{ comment.content }}
-          </p>
-        </div>
-      </div>
+      <CommentItem :comment="comment" />
       <Separator />
     </div>
   </div>
