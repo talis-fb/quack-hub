4ae03bbab20b020aaa3936930d7865ba92114082
Joab Urbano <joab.uaraujo@gmail.com>
2024-05-07 11:01:33 -0300

    [FRONTEND] feature(Comments): Idealização dos comentarios da postagem

---
+++ frontend/src/components/Comments.vue
@@ -0,0 +1,14 @@
+<script></script>
+<template>
+    <div class="flex justify-between items-center gap-1 mt-3">
+        <div class="flex items-center gap-2 pl-2">
+          <img
+            class="rounded-full w-12"
+            src="../assets/DefaultUserIcon.jpg"
+            alt="User profile comment"
+          />
+          <p>Texto do comentario aqui</p>
+        </div>
+        <ThumbsUp class="hover:cursor-pointer hover:text-orange-400" />
+      </div>
+</template>
