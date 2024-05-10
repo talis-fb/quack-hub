d57a3de16676a700ecd8f5c74ace55497b7bb079
Joab Urbano <joab.uaraujo@gmail.com>
2024-05-10 19:02:35 -0300

    [FRONTEND]feature(Barra de pesquisa): Ajustes na cor do botão

--- frontend/src/views/NavMenu.vue
+++ frontend/src/views/NavMenu.vue
@@ -166,6 +166,7 @@ watchEffect(async () => {
 .input-search {
   height: 50px;
   width: 50px;
+  background-color: hsl(var(--muted));
   border-style: none;
   padding: 10px;
   font-size: 18px;
@@ -173,7 +174,6 @@ watchEffect(async () => {
   outline: none;
   border-radius: 25px;
   transition: all .5s ease-in-out;
-  background-color: rgb(204, 142, 26);
   padding-right: 40px;
   color: #fff;
 }
@@ -197,22 +197,21 @@ watchEffect(async () => {
   position: absolute;
   right: 0px;
   color: #ffffff;
-  background-color: transparent;
   pointer-events: painted;
 }
 
 .btn-search:focus ~ .input-search {
   width: 300px;
-  border-radius: 0px;
+  background-color: hsl(var(--muted));
+  border-radius: 1.3rem;
   background-color: transparent;
-  border-bottom: 1px solid rgba(255,255,255,.5);
   transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
 }
 
 .input-search:focus {
   width: 300px;
-  border-radius: 0px;
-  background-color: transparent;
+  border-radius: 1.3rem;
+  background-color: hsl(var(--muted));
   border-bottom: 1px solid rgba(255,255,255,.5);
   transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
 }
