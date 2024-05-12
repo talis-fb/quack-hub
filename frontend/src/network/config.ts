c6854ff801bb481789bfe3d6785aa7c2dfcc6a53
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-05-11 21:46:06 -0300

    hotfix: correção url base padrão usada no frontend

--- frontend/src/network/config.ts
+++ frontend/src/network/config.ts
@@ -1,4 +1,4 @@
-export const baseURL = import.meta.env.VITE_VUE_APP_API_BASE_URL || 'http://localhost:8080/api/v1'
+export const baseURL = import.meta.env.VITE_VUE_APP_API_BASE_URL || '/api/v1'
 
 export const GITHUB_URL = 'https://api.github.com'
 
