d1e4457e228074200c1005d92d62570f176d936c
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-14 21:51:01 -0300

    [FRONTEND] feature(NewsList): criar componente e seu fallback

---
+++ frontend/src/components/NewsList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// Types
+import type { INewsEntity } from '@/entites/INews'
+
+// Services
+import { newsService } from '@/services'
+
+// Vue imports
+import { ref } from 'vue'
+
+const news = ref<INewsEntity[]>([])
+
+async function fetchNews() {
+  const res = await newsService.getNews()
+
+  news.value = res
+}
+
+await fetchNews()
+</script>
+
+<template>
+  <ul>
+    <li>Noticia 01</li>
+    <li>Noticia 02</li>
+    <li>Noticia 03</li>
+  </ul>
+</template>
+<style scoped></style>
---
+++ frontend/src/components/NewsListFallback.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+export interface NewsListFallbackProps {
+  length?: number
+}
+
+const props = withDefaults(defineProps<NewsListFallbackProps>(), {
+  length: 1
+})
+
+
+</script>
+
+<template>
+  <div>
+    <h2>NewListFallback! {{ length }}</h2>
+  </div>
+</template>
+<style scoped></style>
