d0d7975dc2a5e35c0d117ebf8660c2324c9d0ef3
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-16 18:58:39 -0300

    [FRONTEND] feature(announcements): terminar design

--- frontend/src/views/AnnouncementView.vue
+++ frontend/src/views/AnnouncementView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex-1">
-    <div class="w-[800px] max-w-[100%] mx-auto h-screen grid grid-cols-1 md:grid-cols-2 gap-7 p-8">
+    <div class="w-[1200px] max-w-[100%] mx-auto h-screen grid grid-cols-1 md:grid-cols-2 gap-7 p-8">
       <section class="max-w-[100%] flex flex-col">
         <h2 class="text-2xl font-bold tracking-wide">Em andamento</h2>
         <div class="flex-1 border rounded-md"></div>
