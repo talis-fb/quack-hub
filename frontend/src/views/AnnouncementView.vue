3cb1d4271972e91e71abeb8121e2d8d1baaaf191
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-16 13:05:35 -0300

    [FRONTEND] feature(AnnouncementView): criar esqueleto da pagina

--- frontend/src/views/AnnouncementView.vue
+++ frontend/src/views/AnnouncementView.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Announcement page!</h1>
+  <div class="flex-1">
+    <div class="w-[800px] max-w-[100%] mx-auto h-screen grid grid-cols-1 md:grid-cols-2 gap-7 p-8">
+      <section class="max-w-[100%] flex flex-col">
+        <h2 class="text-2xl font-bold tracking-wide">Em andamento</h2>
+        <div class="flex-1 border rounded-md"></div>
+      </section>
+
+      <section class="max-w-[100%] flex flex-col">
+        <h2 class="text-2xl font-bold tracking-wide">Encerrados</h2>
+        <div class="flex-1 border rounded-md"></div>
+      </section>
+    </div>
   </div>
 </template>
 <style scoped></style>
