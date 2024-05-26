fe41ded2456728a12e16e95f49b1cafe4ff71b8c
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-05-26 10:43:03 -0300

    [FRONTEND] feature(singin.test): testar se o metodo router push na ofoi chamado caso o login falhe

--- frontend/test/Signin.test.ts
+++ frontend/test/Signin.test.ts
@@ -5,6 +5,7 @@ import { createTestingPinia } from '@pinia/testing'
 import { useAuthStore } from '../src/stores/auth'
 import { useRouter } from 'vue-router'
 import { mockedStore } from './helpers/mocked-store'
+import { HttpException } from '../src/exceptions/HttpException'
 
 vi.mock('vue-router', () => ({
   useRouter: vi.fn(() => ({
@@ -84,7 +85,7 @@ describe('Signin', () => {
     expect(wrapper.html()).toContain('Senha deve ter no mínimo 5 caracteres.')
   })
 
-  test('should redirect to the home page if login is successful.', async () => {
+  test('should redirect to the home page if login is successful', async () => {
     const wrapper = mount(Signin, {
       global: {
         plugins: [createTestingPinia({})]
@@ -110,5 +111,29 @@ describe('Signin', () => {
 
     expect(push).toHaveBeenCalledTimes(1)
     expect(authStore.signin).toHaveBeenCalledTimes(1)
+  })
+
+  test('should not redirect to home page if login is unsuccessful', async () => {
+    const wrapper = mount(Signin, {
+      global: {
+        plugins: [createTestingPinia({})]
+      }
+    })
+
+    const authStore = mockedStore(useAuthStore)
+
+    authStore.signin.mockRejectedValueOnce(new HttpException('E-mail ou senha incorretos.'))
+
+    const inputEmail = wrapper.find('input[name="email"]')
+    const inputPassword = wrapper.find('input[name="password"]')
+
+    await inputEmail.setValue('luizgustavooumbelino@gmail.com')
+    await inputPassword.setValue('123456')
+
+    await wrapper.find('form').trigger('submit')
+
+    await new Promise((resolve) => setTimeout(resolve, 40))
+
+    expect(push).not.toHaveBeenCalled()
   })
 })
